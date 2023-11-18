864a6e65ea0676921298e8585b11cbc2f1c354a3
Gabriela Berasain <facundoaranda67@gmail.com>
2023-11-18 00:28:11 -0300

    IMPORTANTE ¡LEER!

--- app/db/tp_programacion3.sql
+++ app/db/tp_programacion3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-11-2023 a las 01:30:05
+-- Tiempo de generación: 18-11-2023 a las 04:27:14
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -37,8 +37,9 @@ CREATE TABLE `mesas` (
 --
 
 INSERT INTO `mesas` (`estado`, `idMesa`) VALUES
-('Disponible', 1),
-('Disponible', 2);
+('Cerrado', 1),
+('cerrada', 3),
+('pepe', 5);
 
 -- --------------------------------------------------------
 
@@ -48,21 +49,23 @@ INSERT INTO `mesas` (`estado`, `idMesa`) VALUES
 
 CREATE TABLE `pedidos` (
   `idMesa` bigint(20) DEFAULT NULL,
-  `imgMesa` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
   `estado` varchar(50) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
   `nombreCliente` varchar(50) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
   `precio` float DEFAULT NULL,
-  `comentarios` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-  `clave` char(20) DEFAULT NULL,
-  `idPedido` bigint(20) UNSIGNED NOT NULL
+  `puntuacion` int(2) DEFAULT NULL,
+  `comentarios` varchar(66) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `clavePedido` char(20) NOT NULL,
+  `idPedido` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `pedidos`
 --
 
-INSERT INTO `pedidos` (`idMesa`, `imgMesa`, `estado`, `nombreCliente`, `precio`, `comentarios`, `clave`, `idPedido`) VALUES
-(1, 'url', 'Pendiente', 'Lolo', 500, 'Bien', 'GMNUL', 23);
+INSERT INTO `pedidos` (`idMesa`, `estado`, `nombreCliente`, `precio`, `puntuacion`, `comentarios`, `clavePedido`, `idPedido`) VALUES
+(1, 'finalizado', 'Lucas', 101, 8, 'Muy Bueno', 'L7PR6', 1),
+(3, 'finalizado', 'Juan', 900, 9, 'Excelente', '5JP2P', 2),
+(5, 'pendiente', 'Juan', 900, 0, 'Comentario Pendiente', 'GBYO9', 3);
 
 -- --------------------------------------------------------
 
@@ -73,9 +76,10 @@ INSERT INTO `pedidos` (`idMesa`, `imgMesa`, `estado`, `nombreCliente`, `precio`,
 CREATE TABLE `productos` (
   `estado` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
   `tiempo` float DEFAULT NULL,
-  `tipo` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `sector` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
   `nombre` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
   `clavePedido` char(20) NOT NULL,
+  `idUsuario` int(20) DEFAULT NULL,
   `idProducto` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -83,8 +87,12 @@ CREATE TABLE `productos` (
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`estado`, `tiempo`, `tipo`, `nombre`, `clavePedido`, `idProducto`) VALUES
-('Preparacion', 300, 'Cocina', 'MIlanesa', 'GMNUL', 7);
+INSERT INTO `productos` (`estado`, `tiempo`, `sector`, `nombre`, `clavePedido`, `idUsuario`, `idProducto`) VALUES
+('finalizado', 10, 'bartender', 'cuba libre', 'L7PR6', 5, 3),
+('finalizado', 0, 'cocinero', 'milanesa de caballo', '5JP2P', 4, 4),
+('finalizado', 0, 'cocinero', '2 hamburguesas de garbanzo', '5JP2P', 4, 5),
+('finalizado', 0, 'bartender', 'corona', '5JP2P', 5, 6),
+('finalizado', 0, 'bartender', 'daikiri', '5JP2P', 5, 7);
 
 -- --------------------------------------------------------
 
@@ -96,8 +104,8 @@ CREATE TABLE `usuarios` (
   `sueldo` float DEFAULT NULL,
   `sector` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
   `fechaIngreso` date DEFAULT NULL,
-  `nombreUsuario` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-  `idProducto` bigint(20) DEFAULT NULL,
+  `nombreUsuario` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `password` varchar(10) NOT NULL,
   `idUsuario` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -105,9 +113,12 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`sueldo`, `sector`, `fechaIngreso`, `nombreUsuario`, `idProducto`, `idUsuario`) VALUES
-(900, 'cocinero', '2023-11-06', 'Lucas Ramos', NULL, 5),
-(700, 'mozo', '2023-11-06', 'Pedro Rodriguez', NULL, 6);
+INSERT INTO `usuarios` (`sueldo`, `sector`, `fechaIngreso`, `nombreUsuario`, `password`, `idUsuario`) VALUES
+(5000, 'socio', '2023-05-10', 'Pedro Rodriguez', '12345', 1),
+(700, 'mozo', '2023-11-16', 'Lucas Ramos', '5678', 3),
+(800, 'cocinero', '2023-05-10', 'Rodrigo Lopez', '789', 4),
+(800, 'bartender', '2023-11-16', 'Esteban Fox', '1010', 5),
+(800, 'mozo', '2023-11-17', 'Lucas Alon', '1111', 6);
 
 --
 -- Índices para tablas volcadas
@@ -145,13 +156,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `mesas`
 --
 ALTER TABLE `mesas`
-  MODIFY `idMesa` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idMesa` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `idPedido` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `idPedido` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
